546fda2555f26ce09a5b79b17a47d9928456255b
Claire Chen <claire6698@gmail.com>
2022-10-30 04:32:32 +0000

    new build

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,7 @@ function Footer() {
         <div class="container">
           <p class="m-0 text-center text-white">
             <a class="white-text" href = "https://github.com/julianayue/CreativeP3" > https://github.com/julianayue/CreativeP3 </a>
+            <br/> Created by Juliana Yue and Claire Chen
           </p>
         </div>
       </footer>
